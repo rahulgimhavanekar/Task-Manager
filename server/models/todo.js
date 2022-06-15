cfa01936745b179787fa66a5cd815f3bb6caa50b
Rahul Gimhavanekar <rahulgimhavanekar@gmail.com>
2022-06-16 00:12:59 +0530

    created mongoose model for todo

---
+++ server/models/todo.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const todoSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    descripton: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+module.exports = Todo;
