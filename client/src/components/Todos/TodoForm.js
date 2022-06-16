a7e77eca26cb73a0916c4831a16b8d488ae7179a
Rahul Gimhavanekar <rahulgimhavanekar@gmail.com>
2022-06-16 21:11:26 +0530

    feat: Added form validation for todo form

--- client/src/components/Todos/TodoForm.js
+++ client/src/components/Todos/TodoForm.js
@@ -4,6 +4,26 @@ import classes from "./TodoForm.module.css";
 const TodoForm = (props) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [descError, setDescError] = useState(false);
+
+  const titleInputHandler = (e) => {
+    if (e.target.value.length > 51) {
+      setTitleError(true);
+    } else {
+      setTitleError(false);
+      setTitle(e.target.value);
+    }
+  };
+
+  const descriptionInputHandler = (e) => {
+    if (e.target.value.length > 251) {
+      setDescError(true);
+    } else {
+      setDescError(false);
+      setDescription(e.target.value);
+    }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -26,8 +46,14 @@ const TodoForm = (props) => {
             type="text"
             id="title"
             value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={titleInputHandler}
+            required
           />
+          {titleError && (
+            <p className={classes.error_text}>
+              Title should not exceed more than 50 characters
+            </p>
+          )}
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
@@ -35,10 +61,14 @@ const TodoForm = (props) => {
             id="description"
             rows="8"
             value={description}
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
+            onChange={descriptionInputHandler}
+            required
           ></textarea>
+          {descError && (
+            <p className={classes.error_text}>
+              Description should not exceed more than 250 characters
+            </p>
+          )}
         </div>
         <button className={classes.submit}>Add Todo</button>
       </form>
